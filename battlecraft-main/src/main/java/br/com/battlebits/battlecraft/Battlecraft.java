90926e09ff3c968c3a680b2bfd34c1f9b3119b50
Gustavo Inacio <incio.gusta@gmail.com>
2019-09-01 06:53:42 -0300

    Added warp manager instance

--- battlecraft-main/src/main/java/br/com/battlebits/battlecraft/Battlecraft.java
+++ battlecraft-main/src/main/java/br/com/battlebits/battlecraft/Battlecraft.java
@@ -1,11 +1,14 @@
 package br.com.battlebits.battlecraft;
 
+import br.com.battlebits.battlecraft.manager.WarpManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Battlecraft extends JavaPlugin {
 
+    private WarpManager warpManager;
+
     @Override
     public void onEnable() {
-
+        this.warpManager = new WarpManager(this);
     }
 }
