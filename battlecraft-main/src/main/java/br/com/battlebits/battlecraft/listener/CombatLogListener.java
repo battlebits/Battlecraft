053605b98e3175fe9f13786e1cefe0665422374c
Gustavo Inacio <incio.gusta@gmail.com>
2019-09-02 01:14:39 -0300

    Added CombatLog Manager

--- battlecraft-main/src/main/java/br/com/battlebits/battlecraft/listener/CombatLogListener.java
+++ battlecraft-main/src/main/java/br/com/battlebits/battlecraft/listener/CombatLogListener.java
@@ -1,4 +1,58 @@
 package br.com.battlebits.battlecraft.listener;
 
-public class CombatLogListener {
+import br.com.battlebits.battlecraft.event.PlayerDamagePlayerEvent;
+import br.com.battlebits.battlecraft.manager.CombatLogManager;
+import br.com.battlebits.battlecraft.manager.CombatLogManager.CombatLog;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class CombatLogListener implements Listener {
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerDamage(PlayerDamagePlayerEvent e) {
+        Player damager = e.getDamager();
+        Player damaged = e.getDamaged();
+        CombatLogManager.newCombatLog(damaged, damager);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onDeath(PlayerDeathEvent e) {
+        CombatLogManager.removeCombatLog(e.getEntity());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onQuit(PlayerQuitEvent event) {
+        Player p = event.getPlayer();
+        checkCombatLog(p);
+    }
+
+    @EventHandler
+    public void onVoidDamage(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player))
+            return;
+        if (event.getCause() != DamageCause.VOID)
+            return;
+        Player p = (Player) event.getEntity();
+        checkCombatLog(p);
+    }
+
+    private void checkCombatLog(Player p) {
+        CombatLog log = CombatLogManager.getCombatLog(p);
+        if (log == null)
+            return;
+        if (System.currentTimeMillis() < log.getTime()) {
+            Player combatLogger = log.getCombatLogged();
+            if (combatLogger != null)
+                if (combatLogger.isOnline())
+                    p.damage(Double.MAX_VALUE, combatLogger);
+        }
+        CombatLogManager.removeCombatLog(p);
+    }
+
 }
--- battlecraft-main/src/main/java/br/com/battlebits/battlecraft/manager/CombatLogManager.java
+++ battlecraft-main/src/main/java/br/com/battlebits/battlecraft/manager/CombatLogManager.java
@@ -1,4 +1,60 @@
 package br.com.battlebits.battlecraft.manager;
 
+import br.com.battlebits.battlecraft.Battlecraft;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+
 public class CombatLogManager {
+
+    private static String COMBATLOG_PLAYER = "combatlog.player";
+    private static String COMBATLOG_EXPIRE = "combatlog.time";
+
+    public static void newCombatLog(Player damager, Player damaged) {
+        setCombatLog(damager, damaged);
+        setCombatLog(damaged, damager);
+    }
+
+
+    public static void removeCombatLog(Player player) {
+        Battlecraft plugin = Battlecraft.getInstance();
+
+        if (player.hasMetadata(COMBATLOG_PLAYER))
+            player.removeMetadata(COMBATLOG_PLAYER, plugin);
+        if (player.hasMetadata(COMBATLOG_EXPIRE))
+            player.removeMetadata(COMBATLOG_EXPIRE, plugin);
+    }
+
+    private static void setCombatLog(Player player1, Player player2) {
+        Battlecraft plugin = Battlecraft.getInstance();
+
+        removeCombatLog(player1);
+
+        player1.setMetadata(COMBATLOG_PLAYER, new FixedMetadataValue(plugin, player2.getName()));
+        player1.setMetadata(COMBATLOG_EXPIRE, new FixedMetadataValue(plugin, System.currentTimeMillis() + 10000));
+
+    }
+
+    public static CombatLog getCombatLog(Player player) {
+        Battlecraft plugin = Battlecraft.getInstance();
+        String playerName = "";
+        long time = 0l;
+        if (player.hasMetadata(COMBATLOG_PLAYER))
+            playerName = player.getMetadata(COMBATLOG_PLAYER).get(0).asString();
+        if (player.hasMetadata(COMBATLOG_EXPIRE))
+            time = player.getMetadata(COMBATLOG_EXPIRE).get(0).asLong();
+        Player combatLogged = Bukkit.getPlayer(playerName);
+        return new CombatLog(combatLogged, time);
+    }
+
+    @Data
+    @RequiredArgsConstructor
+    public static class CombatLog {
+        private final Player combatLogged;
+        private final long time;
+
+
+    }
 }
