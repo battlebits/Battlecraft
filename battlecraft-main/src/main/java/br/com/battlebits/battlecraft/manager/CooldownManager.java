8cafc46b214025af764c8fa7e24a0a950b514fc3
Diogo Murano <dominguesmurano@gmail.com>
2019-09-15 01:42:05 -0300

    Added cooldown system

--- battlecraft-main/src/main/java/br/com/battlebits/battlecraft/manager/CooldownManager.java
+++ battlecraft-main/src/main/java/br/com/battlebits/battlecraft/manager/CooldownManager.java
@@ -1,4 +1,71 @@
 package br.com.battlebits.battlecraft.manager;
 
-public class CooldownManager {
+import br.com.battlebits.battlecraft.Battlecraft;
+import br.com.battlebits.commons.bukkit.event.update.UpdateEvent;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CooldownManager implements Listener {
+
+    private static Set<Cooldown> cooldowns;
+
+    private CooldownManager() {
+        cooldowns = new HashSet<>();
+        Battlecraft.getInstance().getServer().getPluginManager().registerEvents(this, Battlecraft.getInstance());
+    }
+
+    public static void create() {
+        new CooldownManager();
+    }
+
+    public static void addCooldown(Cooldown cooldown) {
+        Objects.requireNonNull(cooldown, "cooldown can't be null.");
+        cooldowns.add(cooldown);
+    }
+
+    public static void removeCooldown(Cooldown cooldown) {
+        Objects.requireNonNull(cooldown, "cooldown can't be null.");
+        cooldowns.remove(cooldown);
+    }
+
+    public static boolean hasCooldown(String player, String name) {
+        return cooldowns.stream().anyMatch(c -> c.getPlayer().equals(player) && c.getName().equals(name));
+    }
+
+    public static Cooldown getCooldown(String player, String name) {
+        return cooldowns.stream().filter(c -> c.getPlayer().equals(player) && c.getName().equals(name))
+                .findFirst().orElse(null);
+    }
+
+    public static void removeAllCooldowns(String player) {
+        for (Cooldown cooldown : cooldowns.stream().filter(c -> c.getPlayer().equals(player)).collect(Collectors.toSet())) {
+            removeCooldown(cooldown);
+        }
+    }
+
+    @EventHandler
+    public void onUpdateEvent(UpdateEvent event) {
+        if (event.getType() == UpdateEvent.UpdateType.SECOND) {
+            for (Cooldown cooldown : cooldowns.stream().filter(c -> c.getTime() < System.currentTimeMillis())
+                    .collect(Collectors.toSet())) {
+                removeCooldown(cooldown);
+            }
+        }
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Cooldown {
+
+        private String player;
+        private String name;
+        private long time;
+    }
 }
