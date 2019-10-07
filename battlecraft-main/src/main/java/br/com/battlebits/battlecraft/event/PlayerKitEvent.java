20a1ce99a2d950e1d29d6ca6d030e034e543b44b
Gustavo Inacio <incio.gusta@gmail.com>
2019-10-07 05:30:29 -0300

    Added PlayerKitEvent

--- battlecraft-main/src/main/java/br/com/battlebits/battlecraft/event/PlayerKitEvent.java
+++ battlecraft-main/src/main/java/br/com/battlebits/battlecraft/event/PlayerKitEvent.java
@@ -1,4 +1,21 @@
 package br.com.battlebits.battlecraft.event;
 
-public abstract class PlayerKitEvent {
+import br.com.battlebits.battlecraft.event.protection.PlayerProtectionEvent;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+
+public class PlayerKitEvent extends PlayerProtectionEvent {
+
+    @Getter
+    public static final HandlerList handlerList = new HandlerList();
+
+    public PlayerKitEvent(Player who) {
+        super(who);
+    }
+
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
 }
