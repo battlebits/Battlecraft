cdbe7def6ec041bdfbaa1c877515136456983bd8
Victor Vuelma <victorvuelma@outlook.com>
2019-10-30 00:15:30 -0300

    refactor magma ability

--- battlecraft-main/src/main/java/br/com/battlebits/battlecraft/ability/registry/MagmaAbility.java
+++ battlecraft-main/src/main/java/br/com/battlebits/battlecraft/ability/registry/MagmaAbility.java
@@ -1,58 +1,57 @@
 package br.com.battlebits.battlecraft.ability.registry;
 
 import br.com.battlebits.battlecraft.ability.Ability;
-import br.com.battlebits.commons.bukkit.api.item.ItemBuilder;
+import br.com.battlebits.battlecraft.event.PlayerDamagePlayerEvent;
 import br.com.battlebits.commons.bukkit.event.update.UpdateEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import static br.com.battlebits.battlecraft.manager.ProtectionManager.isProtected;
 
-import java.util.Random;
+import java.util.Arrays;
 
 public class MagmaAbility extends Ability {
 
-    private Random random;
+    private static final DamageCause[] MAGMA_NO_DAMAGE_CAUSES =  new DamageCause[]{
+            DamageCause.FIRE_TICK,
+            DamageCause.FIRE,
+            DamageCause.HOT_FLOOR,
+            DamageCause.LAVA
+    };
 
-    public MagmaAbility() {
-        random = new Random();
-    }
+    private static final double DAMAGE_FIRE_CHANCE = 0.25;
+    private static final int DAMAGE_FIRE_DURATION = 90;
 
-    @EventHandler
+    private static final double WATER_DAMAGE = 2.0;
+
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onEntityDamageEvent(EntityDamageEvent event) {
-        if (event.getEntity() instanceof Player && hasAbility((Player) event.getEntity()) && (event.getCause() ==
-                EntityDamageEvent.DamageCause.LAVA || event.getCause().name().contains("FIRE")) && event.getCause() !=
-                EntityDamageEvent.DamageCause.LIGHTNING) {
+        if (event.getEntity() instanceof Player && Arrays.binarySearch(MAGMA_NO_DAMAGE_CAUSES, event.getCause()) > 0
+            && hasAbility((Player) event.getEntity()) ) {
             event.setCancelled(true);
         }
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
-        if (!(event.getEntity() instanceof Player)) {
-            return;
-        }
-        if (!(event.getDamager() instanceof Player)) {
-            return;
-        }
-        final Player player = (Player) event.getEntity();
-        final Player damager = (Player) event.getDamager();
-        if (hasAbility(damager) && random.nextInt(100) <= 25) {
-            player.setFireTicks(Math.max(player.getFireTicks(), 90));
+    public void onPlayerDamagePlayerEvent(PlayerDamagePlayerEvent event) {
+        if (Math.random() <= DAMAGE_FIRE_CHANCE && hasAbility(event.getPlayer())
+                && !isProtected(event.getPlayer()) && !isProtected(event.getDamaged())) {
+            event.getDamaged().setFireTicks(Math.max(event.getDamaged().getFireTicks(), DAMAGE_FIRE_DURATION));
         }
     }
 
     @EventHandler
     public void onUpdate(UpdateEvent event) {
         if (event.getType() == UpdateEvent.UpdateType.SECOND) {
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                if (hasAbility(player) && player.getLocation().getBlock().getType().name().contains("WATER")) {
-                    player.damage(2);
-                }
-            }
+            Bukkit.getOnlinePlayers().stream()
+                    .filter(p -> p.getLocation().getBlock().getType() == Material.WATER)
+                    .filter(p -> hasAbility(p))
+                    .filter(p -> !isProtected(p))
+                    .forEach(p -> p.damage(WATER_DAMAGE));
         }
     }
 
