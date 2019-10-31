f2fb77dc4e042b16a65a19bc92e62a60eebfadc2
Victor Vuelma <victorvuelma@outlook.com>
2019-10-30 23:25:02 -0300

    fix some bugs and add cooldown warn on ninja

--- battlecraft-main/src/main/java/br/com/battlebits/battlecraft/ability/registry/NinjaAbility.java
+++ battlecraft-main/src/main/java/br/com/battlebits/battlecraft/ability/registry/NinjaAbility.java
@@ -1,55 +1,61 @@
 package br.com.battlebits.battlecraft.ability.registry;
 
 import br.com.battlebits.battlecraft.ability.Ability;
+import br.com.battlebits.battlecraft.event.PlayerDamagePlayerEvent;
+import br.com.battlebits.battlecraft.event.warp.PlayerWarpDeathEvent;
+import br.com.battlebits.battlecraft.event.warp.PlayerWarpQuitEvent;
+import br.com.battlebits.battlecraft.manager.CooldownManager;
+import br.com.battlebits.commons.Commons;
+import br.com.battlebits.commons.translate.Language;
+import br.com.battlebits.commons.util.DateUtils;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static br.com.battlebits.battlecraft.manager.ProtectionManager.isProtected;
+import static br.com.battlebits.battlecraft.translate.BattlecraftTranslateTag.*;
+import static br.com.battlebits.commons.translate.TranslationCommon.tl;
 
 public class NinjaAbility extends Ability {
 
-    private static final String NINJA_COOLDOWN = "ninja";
+    private static final int MAX_DISTANCE = 40;
 
-    private Map<Player, Player> ability;
+    private static final String COOLDOWN_KEY = "ninja";
+    private static final long COOLDOWN_DURATION = 4000;
+
+    private BiMap<Player, Player> ability;
 
     public NinjaAbility() {
-        ability = new HashMap<>();
+        this.ability = HashBiMap.create();
     }
 
-    @EventHandler
-    public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
-        if (event.isCancelled()) {
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    public void onPlayerDamagePlayerEvent(PlayerDamagePlayerEvent event) {
+        if (event.getDamaged().hasMetadata("customDamage") || event.getPlayer().hasMetadata("customDamage")) {
             return;
         }
-        if (event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
-            final Player player = (Player) event.getEntity();
-            final Player damager = (Player) event.getDamager();
-            if (player.hasMetadata("customDamage") || damager.hasMetadata("customDamage")) {
-                return;
-            }
-            //Check protect status
-            if (hasAbility(damager)) {
-                this.ability.put(damager, player);
-            }
+        if (!isProtected(event.getPlayer()) && !isProtected(event.getDamaged()) && hasAbility(event.getPlayer())) {
+            this.ability.put(event.getPlayer(), event.getDamaged());
         }
     }
 
     @EventHandler
-    public void onPlayerDeathEvent(PlayerDeathEvent event) {
-        this.ability.remove(event.getEntity());
-        Player killer = event.getEntity().getKiller();
-        if (killer != null) {
-            this.ability.remove(killer);
-        }
+    public void onPlayerWarpDeathEvent(PlayerWarpDeathEvent event) {
+        this.ability.remove(event.getPlayer());
+
+        this.ability.inverse().remove(event.getPlayer());
     }
 
+    @EventHandler
+    public void onPlayerWarpQuitEvent(PlayerWarpQuitEvent event) {
+        this.ability.remove(event.getPlayer());
+
+        this.ability.inverse().remove(event.getPlayer());
+    }
 
     @EventHandler
     public void onPlayerToggleSneakEvent(PlayerToggleSneakEvent event) {
@@ -58,19 +64,27 @@ public class NinjaAbility extends Ability {
             final Player target = this.ability.get(player);
             //Check protect status
             if (target.isOnline() && !isProtected(target)) {
-                if (player.getLocation().distance(target.getLocation()) > 40) {
-                    player.sendMessage("§cO jogador está muito distante!");
+                if (player.getLocation().distance(target.getLocation()) > MAX_DISTANCE) {
+                    Language language = Commons.getLanguage(player.getUniqueId());
+                    player.sendMessage(tl(language, KIT_NINJA_TAG) + tl(language, KIT_NINJA_DISTANT));
                     return;
                 }
                 //Check gladiator
-                if (!hasCooldown(player, NINJA_COOLDOWN)) {
+                if (!hasCooldown(player, COOLDOWN_KEY)) {
                     player.teleport(target.getLocation());
-                    player.getWorld().playSound(player.getLocation(), Sound
-                            .ENTITY_ENDERDRAGON_FLAP, 1.0F, 1.0F);
+
+                    addCooldown(player, COOLDOWN_KEY, COOLDOWN_DURATION);
                     ability.remove(player);
-                    addCooldown(player, NINJA_COOLDOWN, 4000);
+
+                    player.getWorld().playSound(player.getLocation().add(0.0, 0.5, 0.0), Sound
+                            .ENTITY_ENDERDRAGON_FLAP, 1.0F, 1.0F);
                 } else {
-                    //Send cooldown message
+                    Language language = Commons.getLanguage(player.getUniqueId());
+
+                    long cooldownTime = CooldownManager.getCooldown(player, COOLDOWN_KEY).getTime();
+
+                    player.sendMessage(tl(language, KIT_NINJA_TAG) +
+                            tl(language, KIT_NINJA_COOLDOWN, DateUtils.getDifferenceFormat(language, cooldownTime)));
                 }
             }
         }
