b9dc06d71a0a6f60aa3353ca2936c439e587d4f8
Gustavo Inacio <incio.gusta@gmail.com>
2019-11-15 05:32:09 -0300

    Added some debug commands

--- battlecraft-main/src/main/java/br/com/battlebits/battlecraft/command/ScoreCommand.java
+++ battlecraft-main/src/main/java/br/com/battlebits/battlecraft/command/ScoreCommand.java
@@ -3,6 +3,9 @@ package br.com.battlebits.battlecraft.command;
 import br.com.battlebits.battlecraft.Battlecraft;
 import br.com.battlebits.battlecraft.manager.WarpManager;
 import br.com.battlebits.battlecraft.status.StatusAccount;
+import br.com.battlebits.battlecraft.status.WarpStatus;
+import br.com.battlebits.battlecraft.status.ranking.Queue1v1;
+import br.com.battlebits.battlecraft.status.warpstatus.Status1v1;
 import br.com.battlebits.battlecraft.status.warpstatus.StatusSpawn;
 import br.com.battlebits.battlecraft.warp.Warp;
 import br.com.battlebits.commons.bukkit.command.BukkitCommandArgs;
@@ -24,11 +27,16 @@ public class ScoreCommand implements CommandClass {
         StatusAccount status = Battlecraft.getInstance().getStatusManager().get(p.getUniqueId());
         WarpManager warpManager = Battlecraft.getInstance().getWarpManager();
         Warp currentWarp = warpManager.getPlayerWarp(p);
-        if(currentWarp == warpManager.getDefaultWarp()) {
-            StatusSpawn statusSpawn = (StatusSpawn) status.getWarpStatus(currentWarp);
+        WarpStatus warpStatus = status.getWarpStatus(currentWarp);
+        if (warpStatus instanceof  StatusSpawn) {
+            StatusSpawn statusSpawn = (StatusSpawn) warpStatus;
             p.sendMessage("Kills: " + statusSpawn.getKills());
             p.sendMessage("Deaths: " + statusSpawn.getDeaths());
             p.sendMessage("Killstreak: " + statusSpawn.getKillstreak());
+        } else if (warpStatus instanceof Status1v1) {
+            Status1v1 status1v1 = (Status1v1) warpStatus;
+            p.sendMessage("Victory: " + status1v1.getQueueStatus(Queue1v1.NORMAL).getVictory());
+            p.sendMessage("Defeat: " + status1v1.getQueueStatus(Queue1v1.NORMAL).getDefeat());
         }
 
     }
